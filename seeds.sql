08bcb596b9f4965c794ea9bca660c1c4756b8d47
David Gauthier <cuillerethedj@gmail.com>
2021-07-28 18:36:42 -0500

    new

--- seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Department ( name )
-VALUES ('sales');
-INSERT INTO Department ( name )
-VALUES ('Engineering');
-INSERT INTO roles ( title,salary,department_id )
-VALUES ('Front Desk',40000,2);
